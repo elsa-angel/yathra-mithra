a428c1a907b2b80412d6771e30b96db28b6b7c10
Elsa <elsaangel333@gmail.com>
2024-10-02 13:01:35 +0530

    feat(seat_reservation): implement seat reservation ui pages

--- app/Http/Controllers/ScheduleSearchController.php
+++ app/Http/Controllers/ScheduleSearchController.php
@@ -74,46 +74,4 @@ class ScheduleSearchController extends Controller
     }
 }
 
-
-
-// Fetch the schedules based on the form data
-//$schedules = Schedule::all();
-
-// $schedules = Schedule::where(function ($query) use ($data) {
-//     $query->where('stops', 'LIKE', "%{$data['from']}%")
-//           ->where('stops', 'LIKE', "%{$data['to']}%");
-// })
-// ->with('bus')
-// ->get();
-
-// foreach ($schedules as $schedule) {
-//     // Do something with each $schedule
-
-// }
-
-// where('departure_city', $data['from'])
-// ->where('destination_city', $data['to'])
-// ->whereDate('departure_date', $data['date'])
-// ->whereTime('departure_time', $data['time'])
-// ->get()
-// ->map(function ($schedule) {
-//     // Format the schedule data as needed by your React component
-//     return [
-//         'departure_city' => $schedule->departure_city,
-//         'destination_city' => $schedule->destination_city,
-//         'departure_date' => $schedule->departure_date->format('Y-m-d'),
-//         'departure_time' => $schedule->departure_time->format('H:i'),
-//         'duration' => $schedule->duration,
-//         'fare' => $schedule->fare,
-//     ];
-// });
-
-// Return the data using Inertia
-// return Inertia::render('ScheduleList', [
-//     'auth' => [
-//         'user' => auth()->user(),
-//     ],
-//     'schedules' => $schedules,
-// ]);
-
 // dd($schedules);
